8843dbdbe0aef3bbc7b2aa39b406139c68a7615f
Lillian Dixon <dixon.lillian92@gmail.com>
2020-03-20 15:36:34 -0600

    03202020

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -9,8 +9,9 @@ let randomImage = images[Math.floor(Math.random() * images.length)]
 export default class About extends Component {
     render() {
         return(
-            <div id="about" style={{backgroundImage: 'url(' + randomImage + ')'}}>
+            <div id="about" >
                 <div className="about-text">
+                
                     <h1>About Me</h1>
                     <p>By day I am a Web Developer. By night... I sleep, it's one of my favorite things. I have many varied interest among which are: musical theater, opera, watercolor, knitting, baking, reading everything I can get my hands on, and the occasional Irish step dance. I love learning new things and meeting new people.</p>
                 </div>
