6b2148105634af273f09372e1dbf2b7f4743290e
Lillian Dixon <dixon.lillian92@gmail.com>
2020-06-11 13:51:52 -0600

    project page styles

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -11,45 +11,65 @@ export default function(){
                 <h1 >Projects</h1>
             </div>
             <div className="project-items">
-                <a href='https://joanne-hall-art.herokuapp.com/' >
+
+                {/* <div className='project-item'> */}
+                {/* <a href='https://joanne-hall-art.herokuapp.com/' > */}
                     {/* <div className="link-items"> */}
-                        <div className='background-image' style={{backgroundImage: 'url(' + JoanneHallArt + ')'}}>
+                        {/* <div className='background-image' style={{backgroundImage: 'url(' + JoanneHallArt + ')'}}> */}
                     
-                        </div>
-                        <div className="text-wrapper">
-                            <h2>Joanne Hall Art</h2>
-                            <p>This website was created for a local artist, She wanted somewhere to show off past works of art plus pieces she was currently working on. I created this site in React and SCSS and a backend in Python and Flask.  I built several features for this site including; CRUD, Login and email.</p>
-                        </div>
+                        {/* </div> */}
+                {/* </a> */}
+                        {/* <div className="text-wrapper"> */}
+                            {/* <h2>Joanne Hall Art</h2> */}
+                            {/* <p>This website was created for a local artist, She wanted somewhere to show off past works of art plus pieces she was currently working on. I created this site in React and SCSS and a backend in Python and Flask.  I built several features for this site including; CRUD, Login and email.</p> */}
+                        {/* </div> */}
                     {/* </div> */}
-                </a>
-                <a href='https://li-book-index.herokuapp.com/' >
-                    <div className="background-image" style={{backgroundImage: 'url(' + BookIndex + ')'}}>
-                        
-                    </div>
-                    <div className="text-wrapper">
-                        <h2>Book Index</h2>
-                        <p>This was a project I built to practice my RESTful API's. It has full CRUD capabilities connecting to a python backend I built.  Go ahead and add your favorite book, I'm always looking for new things to read.</p>
-                    </div>
-                </a>
-                
-                <a href='https://dev-fries.herokuapp.com/' >
-                    <div className="background-image" style={{backgroundImage: 'url(' + DevFries + ')'}}>
-                        
-                    </div>
-                    <div className="text-wrapper">
-                        <h2>DevCamp Fries</h2>
-                        <p>A group collaboration of a mock-up restaurant.  This site was created in HTML and CSS implementing experience such as Flexbox and Grid.</p>
-                    </div>
-                </a>
-                <a href='https://javascript-pong-game.herokuapp.com/' >
-                    <div className="background-image" style={{backgroundImage: 'url(' + Pong + ')'}}>
-                        
-                    </div>
-                    <div className="text-wrapper">
-                        <h2>Javascript Pong Game</h2>
-                        <p>This project was super fun to build. It is a simple pong game built entirely in HTML and Javascript. It was a small coding exersise that I had a great time doing.</p>
-                    </div>
-                </a>
+                {/* </div> */}
+
+                <div className='project-item'>
+                    <a href='https://li-book-index.herokuapp.com/' >
+                        <div className="background-image" style={{backgroundImage: 'url(' + BookIndex + ')'}}>
+                            
+                        </div>
+                    </a>
+                        <div className="text-wrapper">
+                            <a href="https://li-book-index.herokuapp.com/">
+                                <h2>Book Index</h2>
+                            </a>
+                            <p>This was a project I built to practice my RESTful API's. It has full CRUD capabilities connecting to a python backend I built.  Go ahead and add your favorite book, I'm always looking for new things to read.</p>
+                            
+                        <a href="https://www.youtube.com/playlist?list=PLZpl_iok1mPZKE_1UCgtilnY83gCf-Vti">Check out how I made it</a>
+                        </div>
+                </div>
+                    
+                <div className='project-item'>
+                    <a href='https://dev-fries.herokuapp.com/' >
+                        <div className="background-image" style={{backgroundImage: 'url(' + DevFries + ')'}}>
+                            
+                        </div>
+                    </a>
+                        <div className="text-wrapper">
+                            <a href="https://dev-fries.herokuapp.com/">
+                                <h2>DevCamp Fries</h2>
+                            </a>
+                            <p>A group collaboration of a mock-up restaurant.  This site was created in HTML and CSS implementing experience such as Flexbox and Grid.</p>
+                        </div>
+                </div>
+
+                <div className='project-item'>
+                    <a href='https://javascript-pong-game.herokuapp.com/' >
+                        <div className="background-image" style={{backgroundImage: 'url(' + Pong + ')'}}>
+                            
+                        </div>
+                    </a>
+                        <div className="text-wrapper">
+                            <a href="">
+                                <h2>Javascript Pong Game</h2>
+                            </a>
+                            <p>This project was super fun to build. It is a simple pong game built entirely in HTML and Javascript. It was a small coding exersise that I had a great time doing.</p>
+                        </div>
+                </div>
+
             </div>
         </div>
     )
